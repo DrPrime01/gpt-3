752c6d9c3bd9f632600b69a4f21d228a87d3be34
DrPrime01 <drprime.dev@gmail.com>
2024-10-16 21:49:41 +0100

    feat: implemented the brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,25 @@
 import "./brand.css";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src="/google.png" alt="google" />
+      </div>
+      <div>
+        <img src="/slack.png" alt="slack" />
+      </div>
+      <div>
+        <img src="/atlassian.png" alt="atlassian" />
+      </div>
+      <div>
+        <img src="/dropbox.png" alt="dropbox" />
+      </div>
+      <div>
+        <img src="/shopify.png" alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
